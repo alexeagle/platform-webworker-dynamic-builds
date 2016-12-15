220b808b42a27ceb8053720ab01de37c1b2b6c24
Alex Eagle <alexeagle@google.com>
2016-12-14 20:00:08 -0800

    4acfde4 feature(DEVELOPER.md): add easy way to publish personal snapshot builds

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var VERSION = new Version('2.3.0-7295a5e');
+export const VERSION = new Version('0.0.0-PLACEHOLDER');
 //# sourceMappingURL=version.js.map
